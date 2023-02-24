b388bbffe2a1c4ed646baacd9569d037b80b729f
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-02-24 10:29:03 +0100

    implementing chat list

--- src/components/mainapp/mainappMainComponent/mainappChat/mainappChatList/MainappChatList.jsx
+++ src/components/mainapp/mainappMainComponent/mainappChat/mainappChatList/MainappChatList.jsx
@@ -1,16 +1,42 @@
 import "./mainappChatList.css";
+import { BiFilter } from "react-icons/bi";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { MdOutlineArchive } from "react-icons/md";
+import { HiLockClosed } from "react-icons/hi";
 import MainappChatHeader from "../mainappChatHeader/MainappChatHeader";
 const MainappChatList = () => {
   return (
     <div className="mainappChatList">
       <div>
         <MainappChatHeader />
-        <div>search</div>
+        <div className="mainappChatList-search">
+          <div className="mainappChatList-search-inner">
+            <div>
+              <BiSearchAlt2 className="mainappChatList-search-icon" />
+            </div>
+            <input type="text" placeholder="Searc or start a new chat" />
+          </div>
+          <div>
+            <BiFilter className="mainappChatList-search-icon-filter" />
+          </div>
+        </div>
       </div>
-      <div>
-        <div>archived</div>
-        <div>list</div>
-        <div>footer</div>
+      <div className="mainappChatList-list-container">
+        <div className="mainappChatList-list-archived">
+          <div>
+            <MdOutlineArchive className="mainappChatList-list-archived-icon" />
+          </div>
+          <div>Archived</div>
+        </div>
+        <div className="mainappChatList-list-chats">{/*map */}</div>
+        <div className="mainappChatList-list-footer">
+          <HiLockClosed />
+          Your personal messages are{" "}
+          <a href="https://en.wikipedia.org/wiki/End-to-end_encryption">
+            {" "}
+            end-to-end encripted
+          </a>
+        </div>
       </div>
     </div>
   );
