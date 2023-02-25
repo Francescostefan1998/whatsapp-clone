cfaba84e6855beae479bdcc2ba8d682533876d30
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-02-25 12:47:04 +0100

    functionality for login and register working
    functionality for login and register working

--- src/components/login/webLogin/loginModal/LoginModal.jsx
+++ src/components/login/webLogin/loginModal/LoginModal.jsx
@@ -10,21 +10,41 @@ const LoginModal = ({ setShowLogin }) => {
   const [showRegistration, setShowRegistration] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastname] = useState("");
+
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
+    console.log("handlesubmit triggered");
+
     try {
-      e.preventDefault();
       const { data } = await axios.post("http://localhost:3001/users/login", {
         email,
         password,
       });
       toast("Login successfull! 💪", { autoClose: 1000 });
+      console.log("login successfull!");
       localStorage.setItem("accessToken", data.accessToken);
       navigate("/home");
     } catch (error) {
       setError(error.response.data.message);
+      console.log("error");
     }
+  };
+  const handleRegister = async (e) => {
+    try {
+      console.log("handleregister triggered");
+      console.log(firstName, lastName, email, password);
+      const data = await axios.post("http://localhost:3001/users", {
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      toast("Register successfull! 💪", { autoClose: 1000 });
+      await handleSubmit();
+    } catch (error) {}
   };
   return (
     <div className="loginModal">
@@ -38,7 +58,11 @@ const LoginModal = ({ setShowLogin }) => {
               <h5>Name</h5>
             </div>
             <div className="input">
-              <input type="text" placeholder="Type here your email" />
+              <input
+                type="text"
+                placeholder="Type here your name"
+                onChange={(val) => setFirstName(val.currentTarget.value)}
+              />
             </div>
           </div>
         )}
@@ -48,7 +72,11 @@ const LoginModal = ({ setShowLogin }) => {
               <h5>Surname</h5>
             </div>
             <div className="input">
-              <input type="text" placeholder="Type here your email" />
+              <input
+                type="text"
+                placeholder="Type here your surname"
+                onChange={(val) => setLastname(val.currentTarget.value)}
+              />
             </div>
           </div>
         )}
@@ -94,7 +122,11 @@ const LoginModal = ({ setShowLogin }) => {
         )}
 
         {showRegistration && (
-          <RegisterModal setShowRegistration={setShowRegistration} />
+          <RegisterModal
+            setShowRegistration={setShowRegistration}
+            handleRegister={handleRegister}
+            previousPassword={password}
+          />
         )}
       </div>
     </div>
--- src/components/login/webLogin/registerModal/RegisterModal.jsx
+++ src/components/login/webLogin/registerModal/RegisterModal.jsx
@@ -1,19 +1,35 @@
 import "./registerModal.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
-const RegisterModal = ({ setShowRegistration }) => {
+const RegisterModal = ({
+  handleRegister,
+  previousPassword,
+  setShowRegistration,
+}) => {
   const navigate = useNavigate();
+  const [password, setPassword] = useState("");
 
+  const handleRegistration = async () => {
+    if (password === previousPassword) {
+      handleRegister();
+    } else {
+    }
+  };
   return (
     <div className="loginModal-password">
       <div className="password">
         <h5>Confirm Password</h5>
       </div>
       <div className="input">
-        <input type="password" placeholder="Type here your password" />
+        <input
+          type="password"
+          placeholder="Type here your password"
+          onChange={(val) => setPassword(val.currentTarget.value)}
+        />
       </div>
       <div className="loginModal-login" id="registerModal-center">
-        <button className="button" onClick={() => navigate("/home")}>
+        <button className="button" onClick={handleRegistration}>
           <h5>Register</h5>
         </button>
       </div>
