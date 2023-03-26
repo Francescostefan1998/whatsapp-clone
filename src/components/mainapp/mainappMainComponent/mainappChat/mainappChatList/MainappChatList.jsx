b35ba3f2161ef71972dfa0e1de812b9c9a655d08
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-03-26 17:13:44 +0200

    add solet.io

--- src/components/mainapp/mainappMainComponent/mainappChat/mainappChatList/MainappChatList.jsx
+++ src/components/mainapp/mainappMainComponent/mainappChat/mainappChatList/MainappChatList.jsx
@@ -4,6 +4,7 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { MdOutlineArchive } from "react-icons/md";
 import { HiLockClosed } from "react-icons/hi";
 import { useSelector } from "react-redux";
+import { ListGroup } from "react-bootstrap";
 import MainappSingleChat from "../mainappSingleChat/MainappSingleChat";
 import MainappChatHeader from "../mainappChatHeader/MainappChatHeader";
 import { getUserChats } from "../../../../../redux/actions";
@@ -13,12 +14,52 @@ import { IoCloseSharp } from "react-icons/io5";
 import ShowProfileInfo from "../../showProfileInfo/ShowProfileInfo";
 import { IoIosArrowForward } from "react-icons/io";
 import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { ListGroupItem } from "react-bootstrap";
+
+const socket = io("http://localhost:3001", { transports: ["websocket"] });
+
 const MainappChatList = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
   const [showProfile, setShowProfile] = useState(false);
-
+  const [login, setLogIn] = useState(false);
+  const [onlineusers, setOnlineUsers] = useState([]);
   const [closeAlert, setCloseAlert] = useState(true);
+  const [chatHistory, setChatHistory] = useState([]);
+
+  useEffect(() => {
+    socket.on("welcome", (welcomeMessage) => {
+      console.log(welcomeMessage);
+      socket.on("loggedIn", (onlineUsersList) => {
+        console.log("logged in", onlineUsersList);
+        setLogIn(true);
+        setOnlineUsers(onlineUsersList);
+      });
+      socket.io("updateOnlineUsers", (onlineUsersList) => {
+        console.log("updateOnlineUsers a new user connectef");
+        setOnlineUsers(onlineUsersList);
+      });
+    });
+    socket.on("newMessage", (newMessage) => {
+      console.log(newMessage);
+      setChatHistory([...chatHistory, newMessage.message]);
+    });
+    submitUsername();
+  }, []);
+
+  const submitUsername = () => {
+    socket.emit("setUsername", user.name);
+  };
+  const sendMessage = () => {
+    const newMessage = {
+      sender: user.name,
+      text: "j",
+      ceatedAt: new Date().toLocaleString("en-US"),
+    };
+    socket.emit("sendMessage", { message: newMessage });
+    setChatHistory([...chatHistory, newMessage]);
+  };
   useEffect(() => {
     if (user) {
       console.log(user);
@@ -88,6 +129,18 @@ const MainappChatList = () => {
               key={chat._id}
             />
           ))}
+          {onlineusers.length === 0 &&
+            (<ListGroup.Item>Log in to check who is online</ListGroup.Item>)()}
+          <ListGroup>
+            {onlineusers.map((user) => (
+              <ListGroup.Item key={user.socketId}>{"k"}</ListGroup.Item>
+            ))}
+          </ListGroup>
+          <ListGroup>
+            {chatHistory.map((message, index) => (
+              <ListGroup.Item key={index}>{message.sender}</ListGroup.Item>
+            ))}
+          </ListGroup>
         </div>
         <div className="mainappChatList-list-footer">
           <HiLockClosed />
