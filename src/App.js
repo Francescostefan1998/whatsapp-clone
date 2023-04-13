7228516797c26d19cb3ff78cecc6eed0e63fd260
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-04-13 12:46:03 +0200

    removing uneccessary functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WebLoginBackground from "./components/login/webLogin/webLoginBackground/WebLoginBackground";
 import MainappBackground from "./components/mainapp/mainappBackground/MainappBackground";
-import MainappDisplayConversationMobile from "./components/mainapp/mainappMainComponent/mainAppDisplayConversationMobile/MainappDisplayConversationMobile";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
@@ -13,10 +12,6 @@ function App() {
         <Routes>
           <Route path="/login" element={<WebLoginBackground />} />
           <Route path="/home" element={<MainappBackground />} />
-          <Route
-            path="/displayconversation"
-            element={<MainappDisplayConversationMobile />}
-          />
         </Routes>
       </BrowserRouter>
     </div>
